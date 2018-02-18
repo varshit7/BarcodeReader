cc9e4fc80707af556d8ad71d0779e87bc60e7ad4
varshit ratna <varshitratna@varshits-MacBook-Pro.local>
2018-02-18 17:15:06 +0530

    Initial commit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/ClickBaitStudio/clickbaitstudio/clickbaitstudio/barcodereader/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ClickBaitStudio.clickbaitstudio.clickbaitstudio.barcodereader.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mkdevelopers.co.in.barcodereader.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
